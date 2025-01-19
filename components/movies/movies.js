5e12be5182672b100459246efdf6c09c6f6d1e06
RPraveen444 <lamechpraveen@gmail.com>
2025-01-19 11:20:09 +0530

    listing all movies and and enable search option

--- components/movies/movies.js
+++ components/movies/movies.js
@@ -1,32 +1,69 @@
 angular.module('movies', [])
-.component('movies', {
-    templateUrl: 'components/movies/movies.html',
-    controller: ['$http', function ($http) {
-        const vm = this;
-        vm.movies = [];
+    .component('movies', {
+        templateUrl: 'components/movies/movies.html',
+        controller: ['$http', '$scope', 'SearchService', function ($http, $scope, SearchService) {
+            const vm = this;
+            vm.movies = [];
+            vm.filteredMovies = [];
+            vm.searchQuery = '';
 
-        const proxyUrl = "https://api.allorigins.win/get?url=";
-        const apiUrl = encodeURIComponent("https://www.freetestapi.com/api/v1/movies");
+            const localImages = [
+                'assets/images/moviesimgs/img1.jpg',
+                'assets/images/moviesimgs/img2.jpg',
+                'assets/images/moviesimgs/img3.jpg',
+                'assets/images/moviesimgs/img4.png',
+                'assets/images/moviesimgs/img5.jpg',
+                'assets/images/moviesimgs/img6.jpg',
+                'assets/images/moviesimgs/img7.jpg',
+                'assets/images/moviesimgs/img8.jpg',
+                'assets/images/moviesimgs/img9.jpg',
+                'assets/images/moviesimgs/img10.jpg',
+                'assets/images/moviesimgs/img12.jpg',
+                'assets/images/moviesimgs/img13.jpg',
+                'assets/images/moviesimgs/img14.jpg',
+                'assets/images/moviesimgs/img15.jpg',
+                'assets/images/moviesimgs/img16.jpg',
+                'assets/images/moviesimgs/img17.jpg',
+                'assets/images/moviesimgs/img18.jpg',
+                'assets/images/moviesimgs/img1.jpg',
+                'assets/images/moviesimgs/img19.jpg',
+                'assets/images/moviesimgs/img20.jpg'
+            ];
+            
+            const proxyUrl = "https://api.allorigins.win/get?url=";
+            const apiUrl = encodeURIComponent("https://www.freetestapi.com/api/v1/movies");
 
-        // Fetching movies using proxy
-        vm.fetchMovies = function () {
-            $http.get(proxyUrl + apiUrl).then(response => {
-                try {
-                    // Parse the actual API response
-                    const parsedData = JSON.parse(response.data.contents);
-                    console.log('Movies Data:', parsedData);
-                    vm.movies = parsedData;
-                } catch (parseError) {
-                    console.error('Error parsing response data:', parseError);
-                }
-            }).catch(error => {
-                console.error('Error fetching movies:', error);
+            // Fetch movies
+            vm.fetchMovies = function () {
+                $http.get(proxyUrl + apiUrl).then(response => {
+                    try {
+                        const parsedData = JSON.parse(response.data.contents);
+                        vm.movies = parsedData.map((movie, index) => ({
+                            ...movie,
+                            poster: localImages[index % localImages.length],
+                        }));
+                        vm.filteredMovies = vm.movies; // Initialize filtered movies
+                    } catch (parseError) {
+                        console.error('Error parsing response:', parseError);
+                    }
+                }).catch(error => {
+                    console.error('Error fetching movies:', error);
+                });
+            };
+
+            // Filter movies when search query changes
+            $scope.$on('searchQueryUpdated', function (event, query) {
+                vm.searchQuery = query;
+                vm.filteredMovies = vm.movies.filter(movie =>
+                    movie.title.toLowerCase().includes(query.toLowerCase())
+                );
             });
-        };
 
-        // Initialize data on component load
-        vm.$onInit = function () {
-            vm.fetchMovies();
-        };
-    }]
-});
+            // Initialize data on component load
+            vm.$onInit = function () {
+                vm.fetchMovies();
+            };
+        }]
+    });
+
+
